d226ddc7a26599549a47339e224496e64f640d8d
arcticcloudfox <27johnson279@gmail.com>
2025-07-18 16:59:43 -0500

    Started redoing TaskController

--- src/main/java/com/tasktracker/controllers/TaskController.java
+++ src/main/java/com/tasktracker/controllers/TaskController.java
@@ -2,9 +2,18 @@ package com.tasktracker.controllers;
 
 
 import com.tasktracker.models.Task;
+import com.tasktracker.models.User;
+import com.tasktracker.models.data.UserRepository;
+import com.tasktracker.security.SecurityConfig;
+import com.tasktracker.service.TaskServiceMethods;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
@@ -12,11 +21,50 @@ public class TaskController {
 
     //redo this to better fit the creation of the task services files
     //also to better fit the templates
+    @Autowired
+     private TaskServiceMethods taskService;
 
-    @GetMapping("/task/{id}")
+    @Autowired
+     private UserRepository userRepository;
+
+    public TaskController(TaskServiceMethods taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks/{id}")
     public String getTaskById(@PathVariable int id, Model model) {
         Task task = (Task) taskService.findTaskById(id);
+        model.addAttribute("task", task);
+        return "details";
     }
+
+    @GetMapping("/tasks/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("task", new Task());
+        return "add";
+    }
+
+    public String getAllTasksByUser(Model model) {
+        List<Task> tasks = taskService.getTasksByUser();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    public String addTask(@ModelAttribute Task task) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+
+        User user = userRepository(currentPrincipalName).orElse(null);
+        assert user != null;
+        Integer userId = user.getId();
+
+        taskService.saveTask(task);
+
+        return "redirect:/" + task.getId();
+    }
+
+    //Finish methods
+    //figure out the errors
 
 
 
